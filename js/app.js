ddd2d8badf3ea165b5395f013a3ce1003d8eff15
Elissa TM <elissawww@gmail.com>
2022-08-07 01:21:28 +0200

    indentacion correcta sin funcion popup

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
   
-  var goUp = $('#goUp');
+    var goUp = $('#goUp');
 
-  $(window).scroll(function() {
-      if ($(window).scrollTop() < 200) {
-        goUp.addClass('show');
-      } else {
-        goUp.removeClass('show');
-      }
-  });
+    $(window).scroll(function() {
+        if ($(window).scrollTop() < 200) {
+            goUp.addClass('show');
+        } else {
+            goUp.removeClass('show');
+        }
+    });
 
-  goUp.on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({scrollTop:0}, '1000');
-  });
+    goUp.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0}, '1000');
+    });
 
 }); 
