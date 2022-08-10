1013c7f08cc3abc3aab097a17fd6f0657ecfa21c
Elissa TM <elissawww@gmail.com>
2022-08-11 01:54:24 +0200

    portfolio completo sin contenido v3

--- js/app.js
+++ js/app.js
@@ -1,28 +1,28 @@
 $(document).ready(function() {
-  
-    let goUp = $('#goUp');
-    let header = $('.header');
 
-    /*muestra u oculta los elementos goUp y header(menu) segun el desplazamiento de ventana*/
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > 200) {
-            goUp.removeClass('showGoUp');
-        } else {
-            goUp.addClass('showGoUp');
-        }
-        if ($(window).scrollTop() > 50) {
-            header.addClass('showBG');
-        } else {
-            header.removeClass('showBG');
-        }
-    });
+	/*muestra u oculta los elementos goUp y header(menu) segun el desplazamiento de ventana*/
+	let goUp = $('#goUp');
+	let header = $('.header');
 
-    /*desplaza contenido al inicio de html*/
-    goUp.click(
-        function(e) {
-            e.preventDefault();
-            $('html, body').animate({scrollTop:0}, '1000');
-        }
-    );
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > 200) {
+			goUp.removeClass('showGoUp');
+		} else {
+			goUp.addClass('showGoUp');
+		}
+		if ($(window).scrollTop() > 50) {
+			header.addClass('showBG');
+		} else {
+			header.removeClass('showBG');
+		}
+	});
+
+	/*desplaza contenido al inicio de html*/
+	goUp.click(
+		function(e) {
+			e.preventDefault();
+			$('html, body').animate({scrollTop:0}, '1000');
+		}
+	);
 
 }); 
