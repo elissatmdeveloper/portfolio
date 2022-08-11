7f702b40587a079132761f41459cf6f32b07f2ec
Elissa TM <elissawww@gmail.com>
2022-08-11 20:25:05 +0200

    nueva imagen

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
 	/*muestra u oculta los elementos goUp y header(menu) segun el desplazamiento de ventana*/
 	let goUp = $('#goUp');
 	let header = $('.header');
+	let item = $('.nav-link');
+	let logo = $('.img-logo');
+	let btnNavbar = $(".navbar-toggler");
 
 	$(window).scroll(function() {
 		if ($(window).scrollTop() > 200) {
@@ -12,8 +15,14 @@ $(document).ready(function() {
 		}
 		if ($(window).scrollTop() > 50) {
 			header.addClass('showBG');
+			item.css("color", "rgb(60, 60, 60)");
+			logo.css("filter", "invert(0)");
+			btnNavbar.css("filter", "invert(0)");
 		} else {
 			header.removeClass('showBG');
+			item.css("color","white");
+			logo.css("filter", "invert(1)");
+			btnNavbar.css("filter", "invert(1)");
 		}
 	});
 
