6f44eb59ecd8d0bded1a98c0e8a293931df431ff
Elissa TM <elissawww@gmail.com>
2022-08-12 04:24:53 +0200

    diseño final pendiente de texto

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+	
 	/*muestra u oculta los elementos goUp y header(menu) segun el desplazamiento de ventana*/
 	let goUp = $('#goUp');
 	let header = $('.header');
@@ -13,19 +14,15 @@ $(document).ready(function() {
 		} else {
 			goUp.addClass('showGoUp');
 		}
-		
+
 		if ($(window).scrollTop() > 50) {
 			header.addClass('showBG');
-			item.css("color", "rgb(60, 60, 60)");
-			logo.css("filter", "invert(0)");
-			btnNavbar.css("filter", "invert(0)");
 		} else {
 			header.removeClass('showBG');
-			item.css("color","white");
-			logo.css("filter", "invert(1)");
-			btnNavbar.css("filter", "invert(1)");
 		}
+
 	});
+
 
 	/*desplaza contenido al inicio de html*/
 	goUp.click(
